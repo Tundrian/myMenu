2a872750c456b1498d2046701ec7b78bf8d17d64
Tundrian <antoniosansotta@gmail.com>
2022-10-11 21:37:38 -0400

    added login, register, home, navbar, and footer components for basic application navigation

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,6 +1,15 @@
 function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <h1>Login</h1>
+      <form className="flex flex-col border border-slate-900" action="">
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
   )
 }
 export default Login
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,6 +1,17 @@
 function Register() {
   return (
-    <div>Register</div>
+    <div>
+      <h1>Register</h1>
+      <form className="flex flex-col border border-slate-900" action="">
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input type="password" name="confirmPassword" id="confirmPassword" />
+        <button type="submit">Register</button>
+      </form>
+    </div>
   )
 }
 export default Register
