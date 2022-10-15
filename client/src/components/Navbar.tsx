14360c066dff118e3f602181e098db9c97d73373
Antonio <antoniosansotta@gmail.com>
2022-10-15 19:26:08 -0400

    added basic styling to splash screen

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,12 +2,13 @@ import { NavLink} from "react-router-dom";
 
 function Navbar( ) {
   return (
-    <div className="sticky top-0 z-50"> 
-      <nav>
-        <ul className="flex justify-around flex-row border border-b-slate-900 mb-10 py-2">
-          <li><NavLink to={`home`} className={({isActive, isPending}) => isActive ? "border-b border-b-green-500" : "border-b border-b-gray-800"}>Home</NavLink></li>
-          <li><NavLink to={`login`} className={({isActive, isPending}) => isActive ? "border-b border-b-green-500" : "border-b border-b-gray-800"}>Login</NavLink></li>
-          <li><NavLink to={`register`} className={({isActive, isPending}) => isActive ? "border-b border-b-green-500" : "border-b border-b-gray-800"}>Register</NavLink></li>
+    <div className="sticky top-0 z-50 py-2 shadow-lg"> 
+      <nav className="flex mx-5">
+        <img className="h-6 aspect-square"  src="logo192.png" />
+        <ul className="w-4/5 flex justify-around flex-row">
+          <li><NavLink to={`home`} className={({isActive, isPending}) => isActive ? "border-b border-b-green-500 bg-slate-500 text-white px-5 rounded-md py-1" : "border-b border-b-gray-800 px-5 py-1"}>Home</NavLink></li>
+          <li><NavLink to={`login`} className={({isActive, isPending}) => isActive ? "border-b border-b-green-500 bg-slate-500 text-white px-5 rounded-md py-1" : "border-b border-b-gray-800 px-5 py-1"}>Login</NavLink></li>
+          <li><NavLink to={`register`} className={({isActive, isPending}) => isActive ? "border-b border-b-green-500 bg-slate-500 text-white px-5 rounded-md py-1" : "border-b border-b-gray-800 px-5 py-1"}>Register</NavLink></li>
         </ul>
       </nav>
     </div>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,13 @@
 function Home() {
   return (
-    <div>
-      <img src="" alt="placeholder splash image" />
-      <h1>Home</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa repudiandae a amet itaque, sit officia quis nobis ex aperiam recusandae.</p>
-      <button>Call to Action</button>  
+    <div className="bg-[url('splash.jpg')] h-screen bg-cover">
+      {/* <img src="splash.jpg" alt="placeholder splash image" /> */}
+      <div className="w-full h-2/3 text-center flex justify-center flex-col align-middle mx-10 text-slate-800">
+        <h1 className="text-7xl mb-5">Home</h1>
+        <p className="max-w-md mx-auto text-slate-900 text-xl bg-white bg-opacity-60">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa repudiandae a amet itaque, sit officia quis nobis ex aperiam recusandae.</p>
+        <button className="py-1 px-2 rounded-md bg-green-600 bg-opacity-80 text-white text-xl w-fit mx-auto mt-5">Call to Action</button>
+      </div>
+        
     </div>
   )
 }
