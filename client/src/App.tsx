5dfae780aa751e3b6409f39fc96ab70b1fca42b7
Antonio <antoniosansotta@gmail.com>
2022-10-17 17:06:08 -0400

    added page and route for browse and menu. Added model for recipe

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { Outlet } from 'react-router-dom';
+
 function App() {
 
   return (
