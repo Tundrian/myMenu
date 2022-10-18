aab0db373a4a4d84e2528b622893c530b49dc355
Tundrian <antoniosansotta@gmail.com>
2022-10-18 18:03:50 -0400

    added api call to browse screen

--- client/src/pages/Browse.tsx
+++ client/src/pages/Browse.tsx
@@ -1,6 +1,30 @@
+import {useEffect, useState} from 'react'
+
 function Browse() {
+
+  const [popular, setPopular] = useState<[any]>([''])
+
+  useEffect(() => {
+    // getPopular()
+  },[])
+
+  const getPopular = async () => {
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+    const data = await api.json()
+    setPopular(data.recipes)
+  }
+
   return (
-    <div>Browse</div>
+    <div>
+      <h1>Browse</h1>
+      {popular.map(recipe => {
+        return(
+          <div key={recipe.id}>
+            <p>{recipe.title}</p>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
