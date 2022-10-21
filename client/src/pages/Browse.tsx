ef3837a3f43ff4674daf093699048ae3ab7bb77b
Antonio <antoniosansotta@gmail.com>
2022-10-20 20:11:04 -0400

    syncing

--- client/src/pages/Browse.tsx
+++ client/src/pages/Browse.tsx
@@ -3,21 +3,43 @@ import {useEffect, useState} from 'react'
 function Browse() {
 
   const [popular, setPopular] = useState<[any]>([''])
-
+  const [search, setSearch] = useState<string>('')
   useEffect(() => {
-    // getPopular()
+    getPopular()
   },[])
 
   const getPopular = async () => {
-    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
-    const data = await api.json()
-    setPopular(data.recipes)
+
+    const check: string= localStorage.getItem('popular') || ''
+
+    if(check !== ''){
+      setPopular(JSON.parse(check))
+    }else{
+      const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+      const data = await api.json()
+      localStorage.setItem('popular', JSON.stringify(data.recipes))
+      setPopular(data.recipes)
+    }
+
+    console.log(popular)
+  }
+
+  const formSubmit = async () => {
+
+  }
+
+  const handleSearchChange = () => {
+
   }
 
   return (
     <div>
       <h1>Browse</h1>
-      {popular.map(recipe => {
+      <form onSubmit={formSubmit}>
+        <input className="border border-slate-900" value={search} onChange={handleSearchChange} />
+
+      </form>
+      {popular && popular.map(recipe => {
         return(
           <div key={recipe.id}>
             <p>{recipe.title}</p>
