1e06c1de0cfaa1408cfb5fa80d4e2a209d4bd199
Tundrian <antoniosansotta@gmail.com>
2022-10-22 14:50:23 -0400

    added search functionality

--- client/src/pages/Browse.tsx
+++ client/src/pages/Browse.tsx
@@ -4,9 +4,15 @@ function Browse() {
 
   const [popular, setPopular] = useState<[any]>([''])
   const [search, setSearch] = useState<string>('')
+  const [sRecipes, setSRecipes] = useState<[any]>([''])
+
   useEffect(() => {
     getPopular()
   },[])
+
+  // useEffect(() => {
+
+  // }, search)
 
   const getPopular = async () => {
 
@@ -24,12 +30,17 @@ function Browse() {
     console.log(popular)
   }
 
-  const formSubmit = async () => {
-
+  const formSubmit = async (e:React.SyntheticEvent) => {
+    e.preventDefault()
+    const api = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${search}&number=9`)
+      const data = await api.json()
+      console.log('data: ', data.results)
+      setSRecipes(data.results)
   }
 
-  const handleSearchChange = () => {
-
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    
+    setSearch(e.target.value)
   }
 
   return (
@@ -37,9 +48,17 @@ function Browse() {
       <h1>Browse</h1>
       <form onSubmit={formSubmit}>
         <input className="border border-slate-900" value={search} onChange={handleSearchChange} />
-
+        <button className="rounded-md py-1 px-4 bg-lime-300 mx-5" type="submit">Search</button>
       </form>
       {popular && popular.map(recipe => {
+        return(
+          <div key={recipe.id}>
+            <p>{recipe.title}</p>
+          </div>
+        )
+      })}
+      <hr />
+      {sRecipes && sRecipes.map(recipe => {
         return(
           <div key={recipe.id}>
             <p>{recipe.title}</p>
